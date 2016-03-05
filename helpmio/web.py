9d230b509e6030c00ba214dd928b354b0cc68159
Dave Korhumel <dpk2442@gmail.com>
2016-03-05 16:22:56 -0500

    add static files

--- helpmio/web.py
+++ helpmio/web.py
@@ -6,8 +6,12 @@ def init(port):
     template_path = os.path.join(
         os.path.dirname(os.path.realpath(__file__)),
         "..", "assets", "templates")
+    static_files_path = os.path.join(
+        os.path.dirname(os.path.realpath(__file__)),
+        "..", "assets", "web")
     app = tornado.web.Application([
         tornado.web.url(r"/", QuestionsHandler, name="main"),
+        tornado.web.url(r"/assets/(.*)", tornado.web.StaticFileHandler, {"path": static_files_path}, name="static")
     ], template_path=template_path, debug=True)
     server = tornado.httpserver.HTTPServer(app)
     server.listen(port)
