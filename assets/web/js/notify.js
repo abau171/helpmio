0860bf4af66a703404d8facc61decd5d763077b7
Dave Korhumel <dpk2442@gmail.com>
2016-03-06 11:39:03 -0500

    hide overlay and start hooking up notifications

--- assets/web/js/notify.js
+++ assets/web/js/notify.js
@@ -2,7 +2,10 @@
     'use strict';
 
     var socket_url,
-        web_socket;
+        web_socket,
+        note_overlay = document.getElementById('note-overlay'),
+        note_panel = document.getElementById('note-panel'),
+        open_notify_dialog = document.getElementById('open-notify-dialog');
 
     if (window.location.protocol === 'https:') {
         socket_url = 'wss:';
@@ -16,5 +19,22 @@
     web_socket.onmessage = function (msg) {
         console.log(msg);
     };
+
+    function toggleNotifyDialog() {
+        if (note_panel.className === 'hidden') {
+            note_panel.className = '';
+            note_overlay.className = '';
+        } else {
+            note_panel.className = 'hidden';
+            note_overlay.className = 'hidden';
+        }
+    }
+
+    note_overlay.addEventListener('click', toggleNotifyDialog);
+
+    open_notify_dialog.addEventListener('click', function (e) {
+        e.preventDefault();
+        toggleNotifyDialog();
+    });
 
 })();
