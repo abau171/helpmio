130797a8dacfe57b32e69597f4427a0f98199fd0
Andrew Bauer <abau171@gmail.com>
2016-03-05 21:36:04 -0500

    simplified chat rooms

--- helpmio/chat.py
+++ helpmio/chat.py
@@ -1,32 +1,29 @@
+import uuid
 import event
 
 class ChatRoom:
 
-	def __init__(self):
-		self._online_sessions = set()
-		self._chats = []
-		self.on_session_connect = event.EventDispatcher()
-		self.on_session_disconnect = event.EventDispatcher()
-		self.on_chat = event.EventDispatcher()
+    def __init__(self):
+        self._connected_users = dict()
+        self._chat_history = []
+        self.on_connect = event.EventDispatcher()
+        self.on_disconnect = event.EventDispatcher()
+        self.on_chat = event.EventDispatcher()
 
-	def connect_session(self, session):
-		self._online_sessions.add(session)
-		self.on_session_connect(session)
+    def connect(self, user):
+        connection_id = str(uuid.uuid4())
+        self._connected_users[connection_id] = user
+        self.on_connect(connection_id)
+        return connection_id
 
-	def disconnect_session(self, session):
-		self._online_sessions.remove(session)
-		self.on_session_disconnect(session)
+    def disconnect(self, connection_id):
+        del self._connected_users[connection_id]
+        self.on_disconnect(connection_id)
 
-	def get__online_sessions(self):
-		return self._online_sessions
+    def add_chat(self, connection_id, text):
+        chat = (connection_id, text)
+        self._chat_history.append(chat)
+        self.on_chat(chat)
 
-	def add_chat(self, session, text):
-		chat = (session["nickname"], text)
-		self._chats.append(chat)
-		self.on_chat(chat)
-
-	def get_chat(self, i):
-		return self._chats[i]
-
-	def get_all_chats(self):
-		return self._chats
+    def get_chat_history(self):
+        return self._chat_history
